6a0a7243ae4a324f5ab5f794b7fde060d20dba35
NEEL VASHIYAR <nv5002@srmist.edu.in>
2026-02-18 10:43:58 +0530

    UC4: Fixed banner alignment using consistent string width

---
+++ UC4BannerApp.java
@@ -0,0 +1,18 @@
+public class UC4BannerApp {
+
+    public static void main(String[] args) {
+
+        String[] banner = {
+                "* * * * * *",
+                "*           *",
+                "*   JAVA    *",
+                "*   UC4     *",
+                "*           *",
+                "* * * * * *"
+        };
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
